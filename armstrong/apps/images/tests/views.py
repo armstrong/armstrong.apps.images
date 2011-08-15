39fcfb971da711314b03b5b222e44b6a79f158c8
Noah Seger <nosamanuel@gmail.com>
2011-08-15 18:11:50 -0500

    All tests succeed

--- armstrong/apps/images/tests/views.py
+++ armstrong/apps/images/tests/views.py
@@ -1,86 +1,86 @@
 import os.path
-from django.test.client import Client
+import shutil
+
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from ._utils import generate_random_image, TestCase
+from django.contrib.sites.models import Site
+
+from armstrong.core.arm_sections.models import Section
+
+from ._utils import generate_random_image, TestCase, PATH_TO_IMG_FILE
 from ..models import Image
 
 
-class BrowseImagesTestCase(TestCase):
-    
+class ImageAdminTestCase(TestCase):
     def setUp(self):
-
         user = User.objects.create_user('admin', 'admin@armstrongcms.org',
-                'admin')
+                                        'admin')
         user.is_superuser = True
         user.is_staff = True
         user.save()
+        self.client.login(username='admin', password='admin')
+        self.images = [generate_random_image() for i in range(10)]
+        self.section = Section.objects.create(title='Test Section')
 
-        self.c = Client()
-        self.c.login(username='admin', password='admin')
-
-        self.images = [generate_random_image() for i in range(10)]    
+    def _tearDown(self):
+        shutil.rmtree(os.path.join(settings.MEDIA_ROOT, 'cache'))
+        shutil.rmtree(os.path.join(settings.MEDIA_ROOT,
+                                   'armstrong', 'test_uploads'))
 
     def test_browse_without_search(self):
-
-        response = self.c.get(reverse('admin:images_admin_browse'))
-
+        response = self.client.get(reverse('admin:images_admin_browse'))
+        self.assertEqual(response.status_code, 200)
         for image in self.images:
             self.assertTrue(image in response.context['image_list'])
 
     def test_browse_unmatching_search(self):
-
         url = '%s?q=%s' % (reverse('admin:images_admin_browse'), 'blahblah')
-        response = self.c.get(url)
+        response = self.client.get(url)
         self.assertEqual(len(response.context['image_list']), 0)
 
     def test_browse_matching_search(self):
-
         self.images[0].title = 'doodaaday'
         self.images[0].save()
 
         url = '%s?q=%s' % (reverse('admin:images_admin_browse'), 'doodaa')
-        response = self.c.get(url)
+        response = self.client.get(url)
         self.assertEqual(len(response.context['image_list']), 1)
         self.assertTrue(self.images[0] in response.context['image_list'])
 
     def test_get_upload_form(self):
-
-        response = self.c.get(reverse('admin:images_admin_upload'))
+        response = self.client.get(reverse('admin:images_admin_upload'))
         self.assertEqual(response.status_code, 200)
 
     def test_upload_image(self):
+        self.assertTrue(not Image.objects.filter(title='uploaded').exists())
+        with open(PATH_TO_IMG_FILE) as image:
+            url = reverse('admin:images_admin_upload')
+            response = self.client.post(url, {
+                    'image': image,
+                    'title': 'uploaded',
+                    'slug': 'uploaded',
+                    'summary': 'uploaded image',
+                    'authors_override': 'bob marley',
+                    'pub_date': '2011-08-15',
+                    'pub_status': 'D',
+                    'tags': 'test tags',
+                    'primary_section': self.section.id,
+                    'sites': Site.objects.get_current().id,
+                    }, follow=True)
 
-        f = open(os.path.join(os.path.dirname(__file__),
-                'images_support/medellin.jpg'))
-
-        data = {
-            'image': f,
-            'title': 'uploaded img',
-            'authors_override': 'bob marley',
-        }
-
-        response = self.c.post(reverse('admin:images_admin_upload'), data, 
-                follow=True)
-        f.close()
-
-        self.assertTrue(Image.objects.filter(title=data['title']).exists())
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(Image.objects.filter(title='uploaded').exists())
 
     def test_render_thumbnail(self):
-
         url = reverse('images_render_thumbnail',
                 kwargs={'pk': self.images[0].id, 'geometry': '100x200'})
-
-        response = self.c.get(url)
-
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
 
     def test_render_original(self):
-
         url = reverse('images_render_original',
-                kwargs={'pk': self.images[0].id})
-
-        response = self.c.get(url)
-
+                      kwargs={'pk': self.images[0].id})
+        response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         self.assertTrue(self.images[0].image.url in response['location'])
