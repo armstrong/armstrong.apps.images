628e1717ddbee947474c48702c91a412cc5f9ed8
Dave McLain <dmclain@texastribune.org>
2011-09-15 12:58:51 -0500

    Organize admin

--- armstrong/apps/images/admin.py
+++ armstrong/apps/images/admin.py
@@ -2,11 +2,23 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.views.generic import DetailView
 
+from armstrong.core.arm_content.admin import fieldsets
+
 from .models import Image
 from .views import BrowseImages, UploadImage
 
 
 class ImageAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'image', 'summary'),
+        }),
+
+        fieldsets.TAXONOMY,
+        fieldsets.PUBLICATION,
+        fieldsets.AUTHORS,
+    )
+
     def get_urls(self):
         urls = super(ImageAdmin, self).get_urls()
         browse_urls = patterns('',
